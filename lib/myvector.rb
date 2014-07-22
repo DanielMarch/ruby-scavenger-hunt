d489aea0bf435957ed6ed3d8f62e88e5dfe1c083
Seth March <seth.d.march@gmail.com>
2014-07-22 15:05:07 -0500

    added cosign and radians

--- lib/myvector.rb
+++ lib/myvector.rb
@@ -1,6 +1,8 @@
 require 'matrix'
 
-class My_Vector < Vector
+attr_reader :cosign, :radian
+
+class MyVector < Vector
   def distance(v)
     Math.sqrt((self[0]-v[0])**2+(self[1]-v[1])**2)
   end
@@ -11,5 +13,21 @@ class My_Vector < Vector
 
   def add_int_vec(v, x)
     v + x
+  end
+
+  # def shovel_int(x)
+  #   self << x
+  # end
+
+  def <<(i)
+    MyVector.elements(self.to_a << i)
+  end
+
+  def cosign(v1, v2)
+    @cosign = (((v1[0] * v2[0]) + (v1[1] * v2[1])) / ((Math.sqrt(v1[0] + v1[2])) * (Math.sqrt(v2[0] + v2[2]))))
+  end
+
+  def radian
+    @radian = cosign * Math::PI / 180
   end
 end
